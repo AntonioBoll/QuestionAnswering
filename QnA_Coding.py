b363c1672dbb0d5620082f50a65aa3ce91c0962e
AntonioBoll <antonioossboll@gmail.com>
2024-03-28 15:40:01 -0300

    Primeira tentativa de Q&A para Python utilizando o banco de dados SquadPT

--- QnA_Coding.py
+++ QnA_Coding.py
@@ -32,3 +32,47 @@ QA_input = {
 }
 res = respostas(QA_input)
 print(res)
+
+# Algum modelo para falar o contexto da imagem e ser passado como contexto do Q&A.
+
+###################### FROM SCRATCH ######################
+# Database obtained from https://forum.ailab.unb.br/t/datasets-em-portugues/251
+import json
+import pandas as pd
+
+# Abrindo o json #
+file_path = "/home/toni/Documents/QuestionAnswering/QuestionAnswering/squad-pt/squad-train-v1.1.json"
+with open(file_path, "r") as json_file:
+    squad_pt = json.load(json_file)
+
+# Transformando o json em dataframe #
+
+# Inicializando listas #
+lista_titulo = []
+lista_contexto = []
+lista_questao = []
+lista_resposta = []
+
+# Obtendo os dados #
+for item in squad_pt['data']:
+    titulo = item['title']
+    for paragraph in item['paragraphs']:
+        contexto = paragraph['context']
+        for qa in paragraph['qas']:
+            questao = qa['question']
+            resposta = qa['answers'][0]['text']
+            
+            # Append data to lists
+            lista_titulo.append(titulo)
+            lista_contexto.append(contexto)
+            lista_questao.append(questao)
+            lista_resposta.append(resposta)
+
+# Criando o dataframe #
+squad_pt = pd.DataFrame({
+    'Title': lista_titulo,
+    'Context': lista_contexto,
+    'Question': lista_questao,
+    'Answer': lista_resposta
+})
+print(squad_pt.head)
